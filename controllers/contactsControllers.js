af16dd600219e8c398e02e955d01c09449c3e4ce
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-04-03 13:00:00 +0300

    add branch main

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,70 +1,11 @@
-import {
-  addContact,
-  getContactById,
-  listContacts,
-  removeContact,
-  updateContactById,
-} from "../services/contactsServices.js";
+import contactsService from "../services/contactsServices.js";
 
-import HttpError from "../helpers/HttpError.js";
+export const getAllContacts = (req, res) => {};
 
-export const getAllContacts = async (req, res, next) => {
-  try {
-    const result = await listContacts();
-    res.status(200).json(result);
-  } catch (error) {
-    next(err);
-  }
-};
+export const getOneContact = (req, res) => {};
 
-export const getOneContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await getContactById(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-};
+export const deleteContact = (req, res) => {};
 
-export const deleteContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await removeContact(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
+export const createContact = (req, res) => {};
 
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const createContact = async (req, res, next) => {
-  try {
-    const result = await addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const updateContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await updateContactById(id, req.body);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    if (Object.keys(req.body).length === 0) {
-      res.status(400).json({ message: "Body must have at least one field" });
-    }
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-};
+export const updateContact = (req, res) => {};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,13 +1,5 @@
 import Joi from "joi";
 
-export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().min(3).max(30).required(),
-  phone: Joi.string().required(),
-});
+export const createContactSchema = Joi.object({});
 
-export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(30),
-  email: Joi.string().min(3).max(30),
-  phone: Joi.string(),
-});
+export const updateContactSchema = Joi.object({});
