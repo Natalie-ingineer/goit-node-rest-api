bb835da00c3e1723d23500c99b9e2f4573867cfc
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-04-03 14:55:48 +0300

    update

--- app.js
+++ app.js
@@ -38,7 +38,3 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
-
-// , () => {
-//   console.log("Server is running. Use our API on port: 3000");
-// });
