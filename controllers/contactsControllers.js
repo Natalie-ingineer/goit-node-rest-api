4fa4d3779f334d6eec9c2b9e37562cfb7fc645aa
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-04-05 11:19:05 +0300

    update

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,12 +1,4 @@
-// import {
-// addContact,
-// getContactById,
-// listContacts,
-// removeContact,
-// updateContactById,
-// } from "../services/contactsServices.js";
-
-// import HttpError from "../helpers/HttpError.js";
+import HttpError from "../helpers/HttpError.js";
 
 import { Contact } from "../db/contact.js";
 import { catchAsync } from "../helpers/catchAsync.js";
@@ -15,50 +7,50 @@ export const getAllContacts = catchAsync(async (req, res) => {
   const result = await Contact.find();
   res.status(200).json(result);
 });
-// export const getOneContact = async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     const result = await getContactById(id);
-//     if (!result) {
-//       throw HttpError(404, "Not found");
-//     }
-//     res.status(200).json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-// export const deleteContact = async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     const result = await removeContact(id);
-//     if (!result) {
-//       throw HttpError(404, "Not found");
-//     }
-//     res.status(200).json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-// export const createContact = async (req, res, next) => {
-//   try {
-//     const result = await addContact(req.body);
-//     res.status(201).json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-// export const updateContact = async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     const result = await updateContactById(id, req.body);
-//     if (!result) {
-//       throw HttpError(404, "Not found");
-//     }
-//     if (Object.keys(req.body).length === 0) {
-//       res.status(400).json({ message: "Body must have at least one field" });
-//     }
-//     res.status(200).json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+
+export const getOneContact = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findById(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+});
+
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndDelete(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+export const createContact = catchAsync(async (req, res) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+});
+
+export const updateContact = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: "Body must have at least one field" });
+  }
+  res.status(200).json(result);
+});
+
+export const updateStatusContact = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: "Body must have at least one field" });
+  }
+  res.status(200).json(result);
+});
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,29 +1,45 @@
 import express from "express";
 import {
   getAllContacts,
-  // getOneContact,
-  // deleteContact,
-  // createContact,
-  // updateContact,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact,
+  updateStatusContact,
 } from "../controllers/contactsControllers.js";
 
-// import validateBody from "../helpers/validateBody.js";
+import validateBody from "../helpers/validateBody.js";
 
-// import {
-//   createContactSchema,
-//   updateContactSchema,
-// } from "../schemas/contactsSchemas.js";
+import { schemasJoi } from "../db/contact.js";
+import { isValidid } from "../middlewares/isValidid.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", getAllContacts);
 
-// contactsRouter.get("/:id", getOneContact);
+contactsRouter.get("/:id", isValidid, getOneContact);
 
-// contactsRouter.delete("/:id", deleteContact);
+contactsRouter.delete("/:id", isValidid, deleteContact);
 
-// contactsRouter.post("/", validateBody(createContactSchema), createContact);
+contactsRouter.post(
+  "/",
+  isValidid,
+  validateBody(schemasJoi.createContactSchema),
+  createContact
+);
 
-// contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
+contactsRouter.put(
+  "/:id",
+  isValidid,
+  validateBody(schemasJoi.updateContactSchema),
+  updateContact
+);
+
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidid,
+  validateBody(schemasJoi.updateFavoriteSchema),
+  updateStatusContact
+);
 
 export default contactsRouter;
